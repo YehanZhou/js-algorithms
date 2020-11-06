f223ef1ec14c56c4a6f03f46742b1e3b2e63ccfb
yehan_zhou <yehan_zhou@kingdee.com>
2020-11-06 10:52:25 +0800

    fin: radix sort

--- sorting/radix-sort.js
+++ sorting/radix-sort.js
@@ -1,50 +1,54 @@
-// todo:很不懂
-const getBucketIndex = (value, minValue, significantDigit, radixBase) =>
-  Math.floor(((value - minValue) / significantDigit) % radixBase);
+const getBucketIndex = (value, significantDigit, radixBase) =>
+  Math.floor((value / significantDigit) % radixBase)
 
 const countingSortForRadix = (array, radixBase, significantDigit, minValue) => {
-  let bucketsIndex;
-  const buckets = [];
-  const aux = [];
+  let bucketsIndex
+  const buckets = []
+  const result = []
   for (let i = 0; i < radixBase; i++) { // 基于基数初始化桶 10个桶
-    buckets[i] = 0;
+    buckets[i] = []
   }
-  // 我们会基于数组中（行{6}）数的有效位（行{7}）进行计数排序（行{8}）
-  for (let i = 0; i < array.length; i++) { // {6}
-    bucketsIndex = getBucketIndex(array[i], minValue, significantDigit, radixBase); // {7}
-    buckets[bucketsIndex]++; // {8}
-  }
-  console.log(buckets)
-  for (let i = 1; i < radixBase; i++) { // {9} 由于我们进行的是计数排序，我们还需要计算累积结果来得到正确的计数值
-    buckets[i] += buckets[i - 1];
-  }
-  console.log(buckets)
-  for (let i = array.length - 1; i >= 0; i--) {
-    bucketsIndex = getBucketIndex(array[i], minValue, significantDigit, radixBase);
-    aux[--buckets[bucketsIndex]] = array[i];
-  }
-  console.log(aux)
+
   for (let i = 0; i < array.length; i++) {
-    array[i] = aux[i];
+    bucketsIndex = getBucketIndex(array[i], significantDigit, radixBase);
+    buckets[bucketsIndex].push(array[i])
   }
-  return array;
+  console.log(JSON.stringify(buckets))
+  for (let i = 0; i < buckets.length; i++) {
+    while(buckets[i].length > 0) {
+      result.push(buckets[i].shift())
+    }
+  }
+  console.log(result)
+  return result
 };
 
 function radixSort(array, radixBase = 10) {
   if (array.length < 2) {
-    return array;
+    return array
   }
-  const minValue = findMinValue(array);
-  const maxValue = findMaxValue(array);
+  const minValue = findMinValue(array)
+  const maxValue = findMaxValue(array)
   // Perform counting sort for each significant digit, starting at 1
-  let significantDigit = 1;
+  let significantDigit = 1
   while ((maxValue - minValue) / significantDigit >= 1) {
     // console.log('radix sort for digit ' + significantDigit);
-    array = countingSortForRadix(array, radixBase, significantDigit, minValue);
+    array = countingSortForRadix(array, radixBase, significantDigit, minValue)
     // console.log(array.join());
-    significantDigit *= radixBase;
+    significantDigit *= radixBase
   }
-  return array;
+  return array
 }
 
-console.log(radixSort([3,4,43,122,56]));
+// console.log(radixSort([3,4,43,122,56]))
+console.log(radixSort([1,19,17,21,33,120]))
+
+/**
+ * 基数排序
+ * 计数排序和桶排序的结合
+ * 按位计算，个->十->百..
+ * 10进制，基数为10，创建10个桶
+ * 将个位值存入对应索引桶（队列）中
+ * 将队列头取出到结果数组
+ * 根据结果数组再计算百位
+ */
